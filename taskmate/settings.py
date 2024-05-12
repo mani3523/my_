03bd4d61a58cf1fc6660796fef20b8eca5fbfad3
mani3523 <manikantanandikolla2@gmail.com>
2024-05-12 18:33:37 +0530

    settings updated

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','mani-todolist-4e38ad9b7156.herokuapp.com/']
+CSRF_TRUSTED_ORIGINS = ['https://mani-todolist-4e38ad9b7156.herokuapp.com/']
 
 
 # Application definition
