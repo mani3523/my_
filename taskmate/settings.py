cbf2271fddef047ad2b657e65eb550f26addda39
mani3523 <manikantanandikolla2@gmail.com>
2024-05-12 17:50:45 +0530

    security key secured

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -26,10 +26,10 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DJANGO_DEBUG', default=False)
+DEBUG = os.environ.get('DJANGO_DEBUG', default=False)
 
 ALLOWED_HOSTS = ['*']
 
