2150d2876163bd7cde0cea24f42fe8224a8cd114
mani3523 <manikantanandikolla2@gmail.com>
2024-05-12 18:23:23 +0530

    small changes in settings

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env()
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', default=False)
+DEBUG = os.environ.get('DJANGO_DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
