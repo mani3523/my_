2a4506e126f81a2e39510c5afe8ccfab0f5b09d1
mani3523 <manikantanandikolla2@gmail.com>
2024-05-14 13:20:13 +0530

    tset

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'taskmate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        #'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'DIRS': ['./templates',],
         'APP_DIRS': True,
         'OPTIONS': {
