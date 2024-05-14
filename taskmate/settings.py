813207135df513e10b9f20a8277ef29156bf6275
mani3523 <manikantanandikolla2@gmail.com>
2024-05-14 13:35:01 +0530

    updated settings

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -65,7 +65,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'DIRS': ['./templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
