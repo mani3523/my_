40298349bc27a03131416d55473fe4ee77ff2cc9
mani3523 <manikantanandikolla2@gmail.com>
2024-05-22 20:17:11 +0530

    heroku deploy

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -99,7 +99,10 @@ WSGI_APPLICATION = 'taskmate.wsgi.application'
 #     }
 # }
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        **dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
